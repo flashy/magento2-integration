01e3ee21a9e1dc1f40d40a56543608fac8289871
Mykola Silin <m.silin@atwix.com>
2023-06-12 14:35:06 +0300

    SRP-147: add new Flashy contact information if subscribed during the checkout flow

--- src/Integration/Observer/Newsletter/SubscriberSaveAfter.php
+++ src/Integration/Observer/Newsletter/SubscriberSaveAfter.php
@@ -7,25 +7,50 @@ use Flashy\Integration\Helper\Data;
 use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Api\Data\AddressInterface;
 use Magento\Customer\Model\ResourceModel\CustomerRepositoryFactory;
+use Magento\Framework\Api\SearchCriteriaBuilderFactory;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Newsletter\Model\Subscriber;
+use Magento\Sales\Api\OrderRepositoryInterfaceFactory;
 
 class SubscriberSaveAfter implements ObserverInterface
 {
-    private $helper;
+    /**
+     * @var CustomerRepositoryFactory
+     */
     private $customerRepositoryFactory;
+
+    /**
+     * @var Data
+     */
+    private $helper;
+
+    /**
+     * @var OrderRepositoryInterfaceFactory
+     */
+    private $orderRepositoryFactory;
+
+    /**
+     * @var SearchCriteriaBuilderFactory
+     */
+    private $searchCriteriaBuilderFactory;
 
     /**
      * @param Data $helper
      * @param CustomerRepositoryFactory $customerRepositoryFactory
+     * @param OrderRepositoryInterfaceFactory $orderRepositoryFactory
+     * @param SearchCriteriaBuilderFactory $searchCriteriaBuilderFactory
      */
     public function __construct(
         Data $helper,
-        CustomerRepositoryFactory $customerRepositoryFactory
+        CustomerRepositoryFactory $customerRepositoryFactory,
+        OrderRepositoryInterfaceFactory $orderRepositoryFactory,
+        SearchCriteriaBuilderFactory $searchCriteriaBuilderFactory
     ) {
         $this->helper = $helper;
         $this->customerRepositoryFactory = $customerRepositoryFactory;
+        $this->orderRepositoryFactory = $orderRepositoryFactory;
+        $this->searchCriteriaBuilderFactory = $searchCriteriaBuilderFactory;
     }
 
     /**
@@ -52,29 +77,44 @@ class SubscriberSaveAfter implements ObserverInterface
         ];
 
         try {
-            /** @var CustomerRepositoryInterface $customerRepository */
-            $customerRepository = $this->customerRepositoryFactory->create();
-            $customerDataModel = $customerRepository->getById((int)$subscriber->getCustomerId());
+            if ($subscriber->getCustomerId()) {
+                /** @var CustomerRepositoryInterface $customerRepository */
+                $customerRepository = $this->customerRepositoryFactory->create();
+                $customerDataModel = $customerRepository->getById((int)$subscriber->getCustomerId());
 
-            $subscriberData['dob'] = $customerDataModel->getDob();
-            $subscriberData['firstname'] = $customerDataModel->getFirstname();
-            $subscriberData['lastname'] = $customerDataModel->getLastname();
+                $subscriberData['dob'] = $customerDataModel->getDob();
+                $subscriberData['firstname'] = $customerDataModel->getFirstname();
+                $subscriberData['lastname'] = $customerDataModel->getLastname();
 
-            $defaultBillingAddress = null;
+                $defaultBillingAddress = null;
 
-            /** @var AddressInterface $address */
-            foreach ($customerDataModel->getAddresses() ?: [] as $address) {
-                if ($address->getId() === $customerDataModel->getDefaultBilling()) {
-                    $defaultBillingAddress = $address;
-                    break;
+                /** @var AddressInterface $address */
+                foreach ($customerDataModel->getAddresses() ?: [] as $address) {
+                    if ($address->getId() === $customerDataModel->getDefaultBilling()) {
+                        $defaultBillingAddress = $address;
+                        break;
+                    }
+                }
+
+                if ($defaultBillingAddress !== null) {
+                    $subscriberData['telephone'] = $defaultBillingAddress->getTelephone();
+                    $subscriberData['city'] = $defaultBillingAddress->getCity();
+                    $subscriberData['street'] = $defaultBillingAddress->getStreet();
+                }
+            } else {
+                $searchCriteria = $this->searchCriteriaBuilderFactory->create();
+                $searchCriteria->addFilter('customer_email', $subscriber->getEmail());
+                $result = $this->orderRepositoryFactory->create()->getList($searchCriteria->create());
+
+                if ($result->getSize()) {
+                    $order = $result->getFirstItem();
+                    $billingAddress = $order->getBillingAddress();
+                    $subscriberData['firstname'] = $billingAddress->getFirstname();
+                    $subscriberData['lastname'] = $billingAddress->getLastname();
+                    $subscriberData['telephone'] = $billingAddress->getTelephone();
                 }
             }
 
-            if ($defaultBillingAddress !== null) {
-                $subscriberData['telephone'] = $defaultBillingAddress->getTelephone();
-                $subscriberData['city'] = $defaultBillingAddress->getCity();
-                $subscriberData['street'] = $defaultBillingAddress->getStreet();
-            }
         } catch (\Exception $e) {
             // TODO: implement logic to handle Exceptions
         }
